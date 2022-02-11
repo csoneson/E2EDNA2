de04503da0efebc89c0ffe5f129d34cdd76cd135
ISDementyev <65086972+ISDementyev@users.noreply.github.com>
2022-02-11 18:04:30 -0500

    Update EXAMPLE_SUB_cluster.sh

--- EXAMPLE_SUB_cluster.sh
+++ EXAMPLE_SUB_cluster.sh
@@ -34,13 +34,14 @@ pip install --no-index lightdock
 pip install --no-index -r requirements.txt
 
 #pip install numpy==1.20.2
-#pip install --no-index numpy==1.20.2  # why didn't Michael use --no-index here?
+#pip install --no-index numpy==1.20.2
 #pip install /cvmfs/soft.computecanada.ca/custom/python/wheelhouse/gentoo/generic/numpy-1.20.2+computecanada-cp37-cp37m-linux_x86_64.whl
 
 #ambertools21 and ambertools20 seems to contain numpy already: numpy-1.21.2
 pip install --no-index --upgrade numpy
 # Without this upgrade, numpy tends to give error for some packages such as mdtraj on beluga (2021-10-08)
 
+# One should add a seqfold, PeptideBuilder and nupack pip installation as wheels, or simply "pip install <package>" depending on the cluster permissions.
 pip install ~/projects/def-simine/programs/opendnaWheels/seqfold-0.7.7-py3-none-any.whl
 pip install ~/projects/def-simine/programs/opendnaWheels/PeptideBuilder-1.1.0-py3-none-any.whl 
 # https://pypi.org/project/PeptideBuilder/1.1.0/
